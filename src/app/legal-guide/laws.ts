b8a460643efcfe305edad70b8497534bad1d287b
Pedro Martins <pedrovrima@gmail.com>
2020-11-08 17:31:54 -0300

    comecei leis

---
+++ src/app/legal-guide/laws.ts
@@ -0,0 +1,17 @@
+export const laws = [
+    {
+      law_number: '9.605',
+      law_date: "12 de fevereiro de 1998",
+      law_text: 'Art. 29. Matar, perseguir, caçar, apanhar, utilizar espécimes da fauna silvestre, nativos ou em rota migratória, sem a devida permissão, licença ou autorização da autoridade competente, ou em desacordo com a obtida:\n\nPena - detenção de seis meses a um ano, e multa.\n\n§ 1º Incorre nas mesmas penas: (...)\n\nIII - quem vende, expõe à venda, exporta ou adquire, guarda, tem em cativeiro ou depósito, utiliza ou transporta ovos, larvas ou espécimes da fauna silvestre, nativa ou em rota migratória, bem como produtos e objetos dela oriundos, provenientes de criadouros não autorizados ou sem a devida permissão, licença ou autorização da autoridade competente.'
+    },
+    // {
+    //   name: 'Phone Mini',
+    //   price: 699,
+    //   description: 'A great phone with one of the best cameras'
+    // },
+    // {
+    //   name: 'Phone Standard',
+    //   price: 299,
+    //   description: ''
+    // }
+  ];
